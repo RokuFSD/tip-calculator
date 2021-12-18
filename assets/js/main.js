9a345ec4a21a6b9db59ebbd34adc706e6661ab39
RokuFSD <emilianordx@gmail.com>
2021-12-18 14:47:37 -0300

    basic styles done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,1 @@
 const mainApp = document.getElementById("tip");
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth >= 720) {
-    mainApp.classList.remove("rounded-top");
-    mainApp.classList.add("rounded");
-  } else {
-    mainApp.classList.add("rounded-top");
-    mainApp.classList.remove("rounded");
-  }
-});
